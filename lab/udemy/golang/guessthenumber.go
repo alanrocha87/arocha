231b0a88a5e4f63312c3dcec1e8bfed6ca6d1027
arocha87 <alanrocha1602@hotmail.com>
2022-02-25 16:24:14 -0500

    curse

---
+++ lab/udemy/golang/guessthenumber.go
@@ -0,0 +1,25 @@
+package main 
+
+
+import "fmt"
+
+func main(){
+
+	var firstnumber = 2 
+	var secondnumber = 5 
+	var subtraction = 7 
+	var answer int 
+	var answer int 
+
+	reader := bufio.New
+
+	fmt.Println("Guess the number game")
+	fmt.Println("Guess the number game")
+	
+
+
+
+
+}
+
+
